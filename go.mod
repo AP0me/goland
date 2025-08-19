435b2647cfc7bb57215a283f62974cc687e5a842
Ap0me <anar.abdullazada@ufaz.az>
2025-08-19 15:16:59 +0400

    db gui

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module desktop
 
 go 1.24.4
 
-require fyne.io/fyne/v2 v2.6.2
+require (
+	fyne.io/fyne/v2 v2.6.2
+	github.com/Masterminds/squirrel v1.5.4
+	github.com/mattn/go-sqlite3 v1.14.32
+)
 
 require (
 	fyne.io/systray v1.11.0 // indirect
@@ -24,6 +28,8 @@ require (
 	github.com/jeandeaual/go-locale v0.0.0-20250612000132-0ef82f21eade // indirect
 	github.com/jsummers/gobmp v0.0.0-20230614200233-a9de23ed2e25 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/nicksnyder/go-i18n/v2 v2.5.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,52 @@
 package main
 
 import (
+	"database/sql"
+	"log"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	sq "github.com/Masterminds/squirrel"
+	_ "github.com/mattn/go-sqlite3"
 )
 
+func E(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func database_connection() (sq.StatementBuilderType, *sql.DB) {
+	db, err := sql.Open("sqlite3", "./gui.db")
+	E(err)
+	psql := sq.StatementBuilder.PlaceholderFormat(sq.Question)
+	return psql, db
+}
+
 func main() {
+	psql, db := database_connection()
+	defer db.Close()
+
+	query, args, err := psql.Select("title", "width", "height").From("window").ToSql()
+	E(err)
+	rows, err := db.Query(query, args...)
+	E(err)
+	defer rows.Close()
+
+	var w_title string = "No Title"
+	var w_width int = 0
+	var w_height int = 0
+	if rows.Next() {
+		err = rows.Scan(&w_title, &w_width, &w_height)
+		E(err)
+	}
+
 	a := app.New()
-	w := a.NewWindow("Hello")
+	w := a.NewWindow(w_title)
+	log.Println(w_width, w_height)
+	w.Resize(fyne.NewSize(float32(w_width), float32(w_height)))
 
 	hello := widget.NewLabel("Hello Fyne!")
 	w.SetContent(container.NewVBox(
