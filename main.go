68ac94e19fee5a6e03a5472318500173a72b51b7
Ap0me <anar.abdullazada@ufaz.az>
2025-08-20 12:30:41 +0400

    widget order

--- main.go
+++ main.go
@@ -29,17 +29,18 @@ func main() {
 	psql, db := database_connection()
 	defer db.Close()
 
-	query, args, err := psql.Select("title", "width", "height").From("window").ToSql()
+	query, args, err := psql.Select("window_id", "title", "width", "height").From("window").ToSql()
 	E(err)
 	rows, err := db.Query(query, args...)
 	E(err)
 	defer rows.Close()
 
+	var window_id int = 0
 	var w_title string = "No Title"
 	var w_width int = 0
 	var w_height int = 0
 	if rows.Next() {
-		err = rows.Scan(&w_title, &w_width, &w_height)
+		err = rows.Scan(&window_id, &w_title, &w_width, &w_height)
 		E(err)
 	}
 
@@ -47,23 +48,23 @@ func main() {
 	w := a.NewWindow(w_title)
 	w.Resize(fyne.NewSize(float32(w_width), float32(w_height)))
 
-
-	query, args, err = psql.Select("title").From("label").ToSql()
+	query, args, err = psql.Select("L.title", "W.widget_order").From("widget AS W").Join("label AS L ON W.widget_id = L.widget_id").Where(sq.Eq{"W.window_id": window_id}).OrderBy("W.widget_order").ToSql()
 	E(err)
 	rows, err = db.Query(query, args...)
 	E(err)
 	defer rows.Close()
 
-	w_content := []fyne.CanvasObject{};
+	w_content := []fyne.CanvasObject{}
 	for rows.Next() {
 		var l_title string = "No Title"
-		err = rows.Scan(&l_title)
+		var widget_order int = 0
+		err = rows.Scan(&l_title, &widget_order)
 		E(err)
 		w_content = append(w_content, widget.NewLabel(l_title))
 	}
 
 	w.SetContent(container.NewVBox(
-		w_content...
+		w_content...,
 	))
 
 	w.ShowAndRun()
